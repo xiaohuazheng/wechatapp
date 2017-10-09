076ea2691d53bfabde87af270c159a34747fa471
xiaohuazheng <463027542@qq.com>
2017-10-09 08:55:48 +0800

    add the circles pages

---
+++ pages/b/c/c.js
@@ -0,0 +1,81 @@
+import _ from '../../../utils/underscore';
+let app = getApp();
+
+Page({
+    data: {
+        shareObj: {
+            share: [{
+                id: 1,
+                name: '飞电风扇',
+                icon: '../../../assets/images/400x140.test.jpg'
+            }, {
+                id: 1,
+                name: '飞电风扇'
+            }, {
+                id: 1,
+                name: '飞电风扇'
+            }, {
+                id: 1,
+                name: '飞电风扇'
+            }]
+        }
+    },
+    onLoad() {
+        app.register('bc', app);
+        //this.getData('new');
+    },
+    getData() {
+        let _this = this;
+        app.util.getCirclesInfo(data => {
+            if (_.isArray(data) && data.length) {
+                for (let i = 0; i < data.length; i++) {
+                    data[i].isTouchMoved = false;
+                }
+                _this.setData({
+                    'shareObj.share': data
+                });
+            }
+        });
+    },
+    onShareAppMessage() {
+        let title = `${(app._user && app._user.nickName || 'GameGuy')} 邀你加入GG游戏圈，我也在，所以更懂你`;
+        return app.util.getShareParams(`/pages/b/c/c`, title);
+    },
+    touchS(e) {
+        this.setData({
+            startX: e.changedTouches[0].clientX,
+            startY: e.changedTouches[0].clientY
+        });
+    },
+    touchM(e) {
+        let _this = this,
+            index = e.currentTarget.dataset.index,
+            startX = this.data.startX,
+            startY = this.data.startY,
+            touchMoveX = e.changedTouches[0].clientX,
+            touchMoveY = e.changedTouches[0].clientY,
+            angle = app.util.angle({
+                X: startX,
+                Y: startY
+            }, {
+                X: touchMoveX,
+                Y: touchMoveY
+            });
+
+        if (Math.abs(angle) > 30) return;
+        if (touchMoveX > startX) {
+            this.data.shareObj.share.forEach((v, i) => {
+                if (v.isTouchMoved) v.isTouchMoved = false;
+            });
+            this.data.shareObj.share[index].isTouchMoved = true;
+            this.setData({
+                shareObj: this.data.shareObj
+            });
+        } else {
+            this.data.shareObj.share[index].isTouchMoved = false;
+            this.setData({
+                shareObj: this.data.shareObj
+            });
+        }
+    }
+});
